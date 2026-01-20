de21bc9d6f0ec88f95a9952afa44a57fc373606b
Cofallen <1727334304@qq.com>
2026-01-20 22:17:17 +0900

    还行的站立

--- DM_H7_Master/User/Leg/vmc.c
+++ DM_H7_Master/User/Leg/vmc.c
@@ -22,15 +22,15 @@ void Vmc_Init(Leg_Typedef *object, float target_l0)
     object->target.d2theta = 0.0f;
 
     // const float F0_control[3] = {8000.0f, 0.0f, 30000.0f};  // 下落状态切换
-    const float F0_control[3] = {600.0f, 1.0f, 1200.0f};
-    const float Yaw_control[3] = {1.0f, 0.0f, 300.0f};
-    const float Delta_control[3] = {10.0f, 0.0f, 10.0f};
-    const float Roll_control[3] = {2.0f, 0.0f, 0.0f};
+    const float F0_control[3] = {800.0f, 1.0f, 1600.0f};
+    const float Yaw_control[3] = {1.0f, 0.0f, 600.0f};
+    const float Delta_control[3] = {20.0f, 0.1f, 50.0f};
+    const float Roll_control[3] = {1000.0f, 0.0f, 30.0f};
 
-    PID_init(&object->pid.F0_l, PID_POSITION, F0_control, 60.0f, 0.0f);
+    PID_init(&object->pid.F0_l, PID_POSITION, F0_control, 80.0f, 0.0f);
     PID_init(&object->pid.Yaw, PID_POSITION, Yaw_control, 1.0f, 0.0f);
-    PID_init(&object->pid.Delta, PID_POSITION, Delta_control, 0.0f, 0.0f);
-    PID_init(&object->pid.Roll, PID_POSITION, Roll_control, 10.0f, 0.0f);
+    PID_init(&object->pid.Delta, PID_POSITION, Delta_control, 1.5f, 0.2f);
+    PID_init(&object->pid.Roll, PID_POSITION, Roll_control, 15.0f, 0.0f);
 }
 
 void Vmc_calcL(Leg_Typedef *object, MOTOR_Typedef *motor, IMU_Data_t *imu, float dt)
